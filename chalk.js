ae4c5df494ff663f7e58b8d774cb654d8bd152db
Jblavisse <jean-baptiste@jblavisse.fr>
2021-05-25 13:45:17 +0200

    Password correct or not?

---
+++ chalk.js
@@ -0,0 +1,7 @@
+const chalk = require("chalk");
+
+console.log(chalk.red("DANGERRRRRRR"));
+console.log("un texte normal");
+console.log(chalk.bold("Un texte en gras"));
+
+console.log(chalk.inverse.red("DANGERR ++++"));
